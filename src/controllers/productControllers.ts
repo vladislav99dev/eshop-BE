c5053a45e4bd7577501ca0d4f9d03de62cb9a94b
Vladislav Dorovski <Vladislav.Dorovski@a1.bg>
2024-02-21 15:19:41 +0200

    add services

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -1,24 +1,31 @@
 import { Request, Response } from "express";
+import productServices from "../services/productServices";
 
 const getAllProducts = (req: Request, res: Response) => {
+  const allProducts = productServices.getAllProducts();
   res.send("Get all Products");
 };
 
 const getOneProduct = (req: Request, res: Response) => {
+  const product = productServices.getOneProduct();
   res.send("Get an existing Product");
 };
 
 const createNewProduct = (req: Request, res: Response) => {
+  const createdProduct = productServices.createNewProduct();
   res.send("Create a new Product");
 };
 
 const updateOneProduct = (req: Request, res: Response) => {
+  const updateProduct = productServices.updateOneProduct();
   res.send("Update an existing Product");
 };
 
 const deleteOneProduct = (req: Request, res: Response) => {
+  const deletedProduct = productServices.deleteOneProduct();
   res.send("Delete an existing Product");
 };
+
 export default {
   getAllProducts,
   getOneProduct,
---
+++ src/services/productServices.ts
@@ -0,0 +1,28 @@
+// In src/services/workoutService.js
+const getAllProducts = () => {
+  return;
+};
+
+const getOneProduct = () => {
+  return;
+};
+
+const createNewProduct = () => {
+  return;
+};
+
+const updateOneProduct = () => {
+  return;
+};
+
+const deleteOneProduct = () => {
+  return;
+};
+
+export default {
+  getAllProducts,
+  getOneProduct,
+  createNewProduct,
+  updateOneProduct,
+  deleteOneProduct,
+};
