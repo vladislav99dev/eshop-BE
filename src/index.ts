cc2e317c6a18c81ca4ad840ee2ce744ce953cacc
Vladislav Dorovski <Vladislav.Dorovski@a1.bg>
2024-02-21 15:21:58 +0200

    add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import productRouter from "./routes/productRoutes";
+import bodyParser from "body-parser";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
 app.use("/api", productRouter);
 
 app.listen(port, () => {
